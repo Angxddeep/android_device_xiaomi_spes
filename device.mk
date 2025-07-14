7ccf7ce4f111427f535db9135ea1594a1e8bacb7
Jooyung Han <jooyung@google.com>
2025-07-14 11:13:32 +0530

    spes: Install apex for clearkey service
    Bug: 329737888
    Test: local build
    Signed-off-by: Angaddeep Singh <angxddeep@gmail.com>
    Change-Id: Ibf5574ce7da0b927ef8179663bb03a039955bcd3

--- device.mk
+++ device.mk
@@ -236,7 +236,7 @@ TARGET_EXCLUDES_AUDIOFX := true
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm-service.clearkey \
+    com.android.hardware.drm.clearkey \
     libcrypto-v33
 
 # Fastbootd
