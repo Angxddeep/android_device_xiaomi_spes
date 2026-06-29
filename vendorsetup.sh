1c14705051a5e0283739dab15fc9c94429b50413
Angaddeep Singh <angxddeep@gmail.com>
2026-06-30 01:14:51 +0530

    Spes: Cleanup Sepolicy edits in vendorsetup.sh
    we use legacy policy already in BoardConfig.mk so this is not needed
    
    Change-Id: I92b5603c39ca31e1a912ee2d7ea7b4ee774f8df7

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,8 +14,3 @@ git clone https://github.com/muralivijay/android_hardware_qcom_audio.git -b line
 echo -e "${color}cloning display HAL ${end}"
 rm -rf hardware/qcom-caf/sm8250/display
 git clone https://github.com/muralivijay/android_hardware_qcom_display.git -b lineage-23.2-caf-sm8250 hardware/qcom-caf/sm8250/display
-
-# Sepolicy fix for imsrcsd
-echo -e "${color}Switch back to legacy imsrcsd sepolicy${end}"
-rm -rf device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/imsservice.te
-cp device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/legacy-ims/hal_rcsservice.te device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/hal_rcsservice.te
