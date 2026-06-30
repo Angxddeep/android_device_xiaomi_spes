367400604b879772a0bc5347aa32b8c43317e072
Angaddeep Singh <angxddeep@gmail.com>
2026-06-30 15:54:31 +0530

    spes: Switch Widevine dependency to libcrypto_shim
    Change-Id: I1eaa9994a67288034e97171389e2beed5c345090

--- extract-files.py
+++ extract-files.py
@@ -61,8 +61,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/camera/components/com.qti.node.mialgocontrol.so': blob_fixup()
         .add_needed('libpiex_shim.so')
         .strip_debug_sections(),
-    'vendor/lib64/libwvhidl.so': blob_fixup()
-        .replace_needed('libcrypto.so', 'libcrypto-v34.so'),
+    ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
+        .add_needed('libcrypto_shim.so'),
     'vendor/lib64/libdpps.so': blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     'vendor/lib64/libgoodixhwfingerprint.so': blob_fixup()
