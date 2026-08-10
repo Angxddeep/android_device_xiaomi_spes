15cbac00461e03893789321c3bcd13b1fca0f785
Bruno Martins <bgcngm@gmail.com>
2026-08-11 00:15:12 +0530

    spes: Address seccomp filter failures
    Fixes: W ATFWD-daemon: libminijail[4334]: logging seccomp filter failures
     E ATFWD-daemon: libminijail[4334]: blocked syscall: gettid
     I init    : Service 'vendor.atfwd' (pid 4334) received signal 31
     I init    : Sending signal 9 to service 'vendor.atfwd' (pid 4334) process group...
    Change-Id: Iad7a17fa0e0b0aa088392efd12c880a8ca98ef5e

--- extract-files.py
+++ extract-files.py
@@ -62,6 +62,8 @@ blob_fixups: blob_fixups_user_type = {
         .strip_debug_sections(),
     ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
+    'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
+        .add_line_if_missing('gettid: 1'),
     'vendor/lib64/libdpps.so': blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     'vendor/lib64/libgoodixhwfingerprint.so': blob_fixup()
